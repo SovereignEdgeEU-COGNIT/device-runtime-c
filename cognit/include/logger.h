22248abe29ef874ab0f805506e995f2992d407a1
Valdes Ivan <ivaldes@ikerlan.es>
2024-01-04 12:04:00 +0100

    Fix logger

--- cognit/include/logger.h
+++ cognit/include/logger.h
@@ -10,15 +10,26 @@
 
 #define COGNIT_LOG_TRACE(...)                       \
     if (COGNIT_LOG_LEVEL <= COGNIT_LOG_LEVEL_TRACE) \
-    printf("[TRACE] %s", __VA_ARGS__)
+        printf("[TRACE] ");                         \
+    printf(__VA_ARGS__);                            \
+    printf("\n");
+
 #define COGNIT_LOG_DEBUG(...)                       \
     if (COGNIT_LOG_LEVEL <= COGNIT_LOG_LEVEL_DEBUG) \
-    printf("[DEBUG] %s", __VA_ARGS__)
+        printf("[DEBUG] ");                         \
+    printf(__VA_ARGS__);                            \
+    printf("\n");
+
 #define COGNIT_LOG_INFO(...)                       \
     if (COGNIT_LOG_LEVEL <= COGNIT_LOG_LEVEL_INFO) \
-    printf("[INFO] %s", __VA_ARGS__)
+        printf("[INFO] ");                         \
+    printf(__VA_ARGS__);                           \
+    printf("\n");
+
 #define COGNIT_LOG_ERROR(...)                       \
     if (COGNIT_LOG_LEVEL <= COGNIT_LOG_LEVEL_ERROR) \
-    printf("[ERROR] %s", __VA_ARGS__)
+        printf("[ERROR] ");                         \
+    printf(__VA_ARGS__);                            \
+    printf("\n");
 
 #endif // LOGGER_HEADER_
