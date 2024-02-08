b246db496208439bb3626599bf82e16d2e0a555e
sperez <sperez@ikerlan.es>
2024-02-08 11:19:38 +0100

    Add minimal offload- example

--- examples/minimal-offload-example.c
+++ examples/minimal-offload-example.c
@@ -1,7 +1,3 @@
-
-
-// TODO
-
 #include <stdio.h>
 #include "cognit_http.h"
 #include <curl/curl.h>
@@ -11,6 +7,8 @@
 #include <unistd.h>
 #include <offload_fc_c.h>
 #include <faas_parser.h>
+#include <cognit_http.h>
+#include <logger.h>
 
 FUNC_TO_STR(
     mult_fc,
@@ -18,9 +16,119 @@ FUNC_TO_STR(
         *c = a * b;
     })
 
-int8_t my_http_send_req_cb(const char* c_buffer, size_t size, http_config_t* config)
+size_t handle_response_data_cb(void* data_content, size_t size, size_t nmemb, void* user_buffer)
 {
-    printf("SEND HERE");
+    size_t realsize           = size * nmemb;
+    http_response_t* response = (http_response_t*)user_buffer;
+
+    if (response->size + realsize >= sizeof(response->ui8_response_data_buffer))
+    {
+        COGNIT_LOG_ERROR("Response buffer too small");
+        return 0;
+    }
+
+    memcpy(&(response->ui8_response_data_buffer[response->size]), data_content, realsize);
+    response->size += realsize;
+    response->ui8_response_data_buffer[response->size] = '\0';
+
+    return realsize;
+}
+
+int my_http_send_req_cb(const char* c_buffer, size_t size, http_config_t* config)
+{
+    CURL* curl;
+    CURLcode res;
+    long http_code             = 0;
+    struct curl_slist* headers = NULL;
+    memset(&config->t_http_response.ui8_response_data_buffer, 0, sizeof(config->t_http_response.ui8_response_data_buffer));
+    config->t_http_response.size = 0;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        // Set the request header
+        headers = curl_slist_append(headers, "Accept: application/json");
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        headers = curl_slist_append(headers, "charset: utf-8");
+
+        if (curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers) != CURLE_OK
+            // Configure URL and payload
+            || curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V6) != CURLE_OK
+            || curl_easy_setopt(curl, CURLOPT_URL, config->c_url) != CURLE_OK
+            // Set the callback function to handle the response data
+            || curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&config->t_http_response) != CURLE_OK
+            || curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_response_data_cb) != CURLE_OK
+            || curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, config->ui32_timeout_ms) != CURLE_OK)
+        {
+            COGNIT_LOG_ERROR("[hhtp_send_req_cb] curl_easy_setopt() failed");
+            return -1;
+        }
+        if (strcmp(config->c_method, HTTP_METHOD_GET) == 0)
+        {
+            if (curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_USERNAME, config->c_username) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_PASSWORD, config->c_password) != CURLE_OK)
+            {
+                COGNIT_LOG_ERROR("[hhtp_send_req_cb] curl_easy_setopt()->get() failed");
+                return -1;
+            }
+        }
+        else if (strcmp(config->c_method, HTTP_METHOD_POST) == 0)
+        {
+            if (curl_easy_setopt(curl, CURLOPT_POST, 1L) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST") != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, size) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_POSTFIELDS, c_buffer) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_USERNAME, config->c_username) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_PASSWORD, config->c_password) != CURLE_OK)
+            {
+                COGNIT_LOG_ERROR("[hhtp_send_req_cb] curl_easy_setopt()->post() failed");
+                return -1;
+            }
+        }
+        else if (strcmp(config->c_method, HTTP_METHOD_DELETE) == 0)
+        {
+            if (curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE") != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_USERNAME, config->c_username) != CURLE_OK
+                || curl_easy_setopt(curl, CURLOPT_PASSWORD, config->c_password) != CURLE_OK)
+            {
+                COGNIT_LOG_ERROR("[hhtp_send_req_cb] curl_easy_setopt()->post() failed");
+                return -1;
+            }
+        }
+        else
+        {
+            COGNIT_LOG_ERROR("[hhtp_send_req_cb] Invalid HTTP method");
+            return -1;
+        }
+
+        // Make the request
+        res = curl_easy_perform(curl);
+
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+        COGNIT_LOG_ERROR("HTTP err code %ld ", http_code);
+
+        // Check errors
+        if (res != CURLE_OK)
+        {
+            long http_code = 0;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+            COGNIT_LOG_ERROR("curl_easy_perform() failed: %s", curl_easy_strerror(res));
+            COGNIT_LOG_ERROR("HTTP err code %ld ", http_code);
+        }
+
+        // Clean and close CURL session
+        curl_easy_cleanup(curl);
+    }
+
+    config->t_http_response.l_http_code = http_code;
+
+    // Clean global curl configuration
+    curl_global_cleanup();
+
+    return (res == CURLE_OK) ? 0 : -1;
 }
 
 int main(int argc, char const* argv[])
@@ -81,6 +189,7 @@ int main(int argc, char const* argv[])
 
     serverless_runtime_ctx_call_sync(&t_my_serverless_runtime_context, &exec_params, &t_exec_response);
 
+    COGNIT_LOG_INFO("Result: %s", t_exec_response.res_payload);
 
     // TODO: wrap all destroys in a single function
     faasparser_destroy_exec_response(&t_exec_response);
